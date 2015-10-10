55ec2218e7880c61afd182cd9004813d4e2570a7
Venkatesh Sambandamoorthy <ven0226@gmail.com>
2015-10-10 16:37:24 -0400

    latest changes

--- jobs/sample.rb
+++ jobs/sample.rb
@@ -1,7 +1,7 @@
 require 'rest_client'
 current_valuation = 0
 current_karma = 0
-$loopCount = 10
+#$loopCount = 10
 $resp_text = ''
 SCHEDULER.every '2s' do
   #url = 'http://www.acme.com/products/3322' # ACME boomerang
@@ -20,12 +20,21 @@ SCHEDULER.every '2s' do
     response.return!(request, result, &block)
   end
   }
-  last_valuation = current_valuation
-  last_karma     = current_karma
-  current_valuation = rand(10)
-  current_karma     = rand(200000)
-  send_event('valuation0', { current: current_valuation, last: last_valuation })
-  send_event('valuation1', { current: current_valuation, last: last_valuation })
-  send_event('karma', { current: current_karma, last: last_karma })
-  send_event('synergy',   { value: rand(100) })
+  i = 0
+  responseOut = JSON.parse(response.body)
+  #@loopCount = responseOut['dashboard']['total'].to_i
+  @interest = responseOut['dashboard']['interest']
+
+  @interest.each do |json|
+    send_event('valuation' + i, { current: json[value].to_i})
+    i = i + 1
+  end
+  #last_valuation = current_valuation
+  #last_karma     = current_karma
+  #current_valuation = rand(10)
+  #current_karma     = rand(200000)
+  #send_event('valuation0', { current: current_valuation, last: last_valuation })
+  #send_event('valuation1', { current: current_valuation, last: last_valuation })
+  #send_event('karma', { current: current_karma, last: last_karma })
+  #send_event('synergy',   { value: rand(100) })
 end
