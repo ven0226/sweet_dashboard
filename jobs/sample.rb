d1614970b61269b6a926185713d3f491285cce3f
Venkatesh Sambandamoorthy <ven0226@gmail.com>
2015-10-11 01:46:15 -0400

    cool dashboard

--- jobs/sample.rb
+++ jobs/sample.rb
@@ -1,50 +1,22 @@
 require 'rest-client'
 require 'nokogiri'
-current_valuation = 0
-current_karma = 0
-#$loopCount = 10
-$resp_text = ''
-SCHEDULER.every '2s' do
-  #url = 'http://www.acme.com/products/3322' # ACME boomerang
-  #resp = Net::HTTP.get_response(URI.parse(url))
-  #$resp_text = resp.body
-  #response = RestClient.get 'http://localhost:3000/dashboard'
-  #response.code
-  response = RestClient.get('http://localhost:3000/dashboard'){ |response, request, result, &block|
-  case response.code
-  when 200
-    #p "It worked !"
-    response
-  when 423
-    raise SomeCustomExceptionIfYouWant
-  else
-    response.return!(request, result, &block)
-  end
-  }
-  i = 0
-  responseOut = JSON.parse(response.body)
-  #@loopCount = responseOut['dashboard']['total'].to_i
+
+SCHEDULER.every '10s' do
+  queryNeo = "start n=node(*) match (n{type:'skill'})-[r]-() return n, count(r) as rel_count order by rel_count desc"
+  #args = {'query': query}
+  response = RestClient.post('http://127.0.0.1:7474/db/data/cypher',
+    {
+      :query => queryNeo
+  })
+
   rows=[]
-  interest = responseOut['interest']
-  #Sinatra::Application::INTERESTS = interest
-  #erb :sample, :locals => {:clients => @interest}
-  interest.each do |json|
-    #p json
-    #p json["key"]
-    #p json["value"]
-    rows << json["key"]
-    send_event(json["key"], { current: json["value"].to_i})
-    i = i + 1
+  responseOut = JSON.parse(response.body)
+  responseOut['data'].each do |resp|
+    key = resp[0]['data']['name']
+    value =  resp[1]
+    rows << key
+    send_event(key, { current: value.to_i})
   end
-  #p rows
   set :number_rows, rows
-  #send_event('refresh_sample', {event: 'reload', dashboard: 'sample'}, 'dashboards')
-  #last_valuation = current_valuation
-  #last_karma     = current_karma
-  #current_valuation = rand(10)
-  #current_karma     = rand(200000)
-  #send_event('valuation0', { current: current_valuation, last: last_valuation })
-  #send_event('valuation1', { current: current_valuation, last: last_valuation })
-  #send_event('karma', { current: current_karma, last: last_karma })
-  #send_event('synergy',   { value: rand(100) })
+  #print responseOut
 end
